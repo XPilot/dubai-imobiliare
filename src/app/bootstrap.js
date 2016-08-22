82e5a09f837e0efcb1181ab81434efbb8b3aa526
Radu Paraschiv <xpilot@gmail.com>
2016-08-22 21:47:55 +0300

    Femeie ma omoara

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,5 +1,9 @@
 // styles
 import 'style/main.scss';
 
+import Wallop from 'wallop';
+
 document.addEventListener('DOMContentLoaded', (ev) => {
+  const sliderEl = document.querySelector('.Slider');
+  const slider = new Wallop(sliderEl);
 });
